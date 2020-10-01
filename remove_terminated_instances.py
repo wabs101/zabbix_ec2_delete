625bc36d22743b7b1c3bc61b8ddb3a0d08da651b
wabs101 <71108624+wabs101@users.noreply.github.com>
2020-10-01 17:22:02 -0230

    Update remove_terminated_instances.py

--- remove_terminated_instances.py
+++ remove_terminated_instances.py
@@ -54,3 +54,5 @@ if zabbix_hosts_ids_to_delete:
     print("These hosts will be deleted from Zabbix: {0}".format(zabbix_hosts_ids_to_delete))
 else:
     print("Nothing to remove")
+    print("I found these {0}".format(ec2_active_instances_IPs))
+    print("I found these too {0}".format(zabbix_unavailable_hosts_IPs))
